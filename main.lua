3bbcfa6b17ed832909d516484d81dddddc9dd679
LuminaProject <krtoowar015@gmail.com>
2026-03-16 21:20:59 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 11729688377 or game.PlaceId == 11879754496 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f46556d2b27dc8145318c07e1b7c859e.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/804101dd926c119c495d1f25cfdf85a6.lua"))()
 end
